fdc3955a544d01e935778ffa5791d93affef78ce
shubhamAW <waykarshubham2022@gmail.com>
2022-11-08 08:51:49 +0530

    add navigation menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        sm:["14px"],
+      }
+    },
   },
   plugins: [],
 }
